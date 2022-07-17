75d9f7074667c5b833dbc83da3fcb1a08f3bdc28
yyt-2378 <3532951485@qq.com>
2022-07-17 18:45:53 +0800

    提交文件

--- stm-code_master_rdfunction.py/rdfunction-update.py
+++ stm-code_master_rdfunction.py/rdfunction-update.py
@@ -4,7 +4,6 @@ from rdkit.Chem import Draw
 from matplotlib import pyplot as plt
 import numpy as np
 import warnings
-import os
 from mpl_toolkits.mplot3d import Axes3D
 
 try:
@@ -18,40 +17,32 @@ import math
 import csv
 
 
-def mkdir(path):
-    folder = os.path.exists(path)
+def nested_list_to_tuple_recur(nested_list):  # 嵌套列表转化为嵌套元组便于集合筛选出重复序列
+    return tuple(
+        nested_list_to_tuple_recur(l) if isinstance(l, list)
+        else l for l in nested_list
+    )
 
-    if not folder:  # 判断是否存在文件夹如果不存在则创建为文件夹
-        os.makedirs(path)  # makedirs 创建文件时如果路径不存在会创建这个路径
-        print("---  new folder...  ---")
-        print("---  OK  ---")
-
-    else:
-        print("---  There is this folder!  ---")
 
 def points_height_matrix(smi, resolution, show=False):
-    mol_3D= Chem.MolFromSmiles(smi)
-    mol_2D= Chem.MolFromSmiles(smi)
-    atom_num = mol_2D.GetNumAtoms()
-    AllChem.EmbedMolecule(mol_3D, randomSeed=3)
-    AllChem.MMFFOptimizeMolecule(mol_3D)  # 2D分子转为3D结构，并用力学函数进行角度校正及优化
-    moleblock = Chem.MolToMolBlock(mol_3D)  # 返回分子中各个原子的三维坐标
-    moleblock_2D=Chem.MolToMolBlock(mol_2D) # 返回分子中各个原子的二维坐标
+    mol = Chem.MolFromSmiles(smi)
+    # 在结构中添加H原子
+    m3d = Chem.AddHs(mol)
+    AllChem.EmbedMolecule(m3d, randomSeed=3)
+    AllChem.MMFFOptimizeMolecule(m3d)  # 2D分子转为3D结构，并用力学函数进行角度校正及优化
+    moleblock = Chem.MolToMolBlock(m3d)  # 返回分子中各个原子的三维坐标
     if show:
-        Draw.ShowMol(mol_3D, size=(550, 550), kekulize=False)
-        Draw.ShowMol(mol_2D,size=(550,550),kekulize=False)
-    mol_2D = Chem.MolFromMolBlock(moleblock_2D)
-    conf = mol_2D.GetConformer()
-    sub = mol_2D.GetSubstructMatch(mol_2D)  # 这一步是统计你所需查找返回分子的个数列表，m3d：是查询分子的smile结构,我们这里使用自查询
+        Draw.ShowMol(m3d, size=(550, 550), kekulize=False)
+    conf = m3d.GetConformer()
+    sub = m3d.GetSubstructMatch(m3d)  # 这一步是统计你所需查找返回分子的个数，m3d：是查询分子的smile结构,我们这里使用自查询
     # 记录每一个原子的位置
     atom_position = []
-    for s in range(atom_num):
+    for s in sub:
         # list(conf.GetAtomPosition(s))#编号为s的原子坐标
         # m3d.GetAtoms()[s].GetSymbol()#编号为s对应的符号,str
         # print(m3d.GetAtoms()[s].GetSymbol(), list(conf.GetAtomPosition(s)))
         x = list(conf.GetAtomPosition(s))
-        mol_2D.GetAtomWithIdx(s).SetProp('molAtomMapNumber', str(mol_2D.GetAtomWithIdx(s).GetIdx())) #对原子进行索引
-        atom_symbol = mol_2D.GetAtoms()[s].GetSymbol()
+        atom_symbol = m3d.GetAtoms()[s].GetSymbol()
         if atom_symbol == 'C':
             x.append(0.77)
         elif atom_symbol == 'H':
@@ -66,9 +57,8 @@ def points_height_matrix(smi, resolution, show=False):
             x.append(0.99)
         else:
             raise NotImplementedError("only support C, H, O, S, N, Cl")
-        x.append(mol_2D.GetAtomWithIdx(s).GetIdx())
         atom_position.append(x)
-    print(atom_position)
+    # print(atom_position)
     # 在三维坐标系绘制得到的原子坐标，可通过原子坐标调整三维坐标轴大小
     if show and ENABLE_MAYAVI:
         plt.figure()  # 得到画面
@@ -104,7 +94,6 @@ def points_height_matrix(smi, resolution, show=False):
     # todo: change tuple operation to numpy operation
     points_intial = np.zeros([resolution, resolution])  # 建立网格点的初始化高度矩阵，与生成点阵坐标一一对应
     # print(x_max, x_min, y_max, y_min)
-    points_bool=np.zeros([atom_num,resolution,resolution])
     x_axes = np.linspace(x_min, x_max, resolution)
     y_axes = np.linspace(y_min, y_max, resolution)
     X, Y = np.meshgrid(x_axes, y_axes)
@@ -113,52 +102,46 @@ def points_height_matrix(smi, resolution, show=False):
     coordinate_points_array = coordinate_points.T
     # print(coordinate_points_array)
     coordinate_points_array = np.array(coordinate_points_array)  # 这里得再去定义一下类型，我也不知道为什么，不然后面无法操作
-    v=coordinate_points_array.reshape(resolution,resolution,2)
+    coordinate_points_list = coordinate_points_array.tolist()
+    coordinate_points_tuple = nested_list_to_tuple_recur(coordinate_points_list)  # 转换为嵌套元组
+    v = [[] for i in range(resolution)]
+    for i in range(resolution):
+        for j in range(resolution):
+            v[i].append(coordinate_points_tuple[resolution * i + j])
 
     # todo: calculate per pixel label of each atom in the image
     # 开始计算分子中各个原子距离基平面的高度，此时我们基平面设为xoy平面，虚拟为通过点光源簇从上向下进行投影，即以接触到的最高的原子的高度作为记录的高度
     for r in range(resolution):
         for t in range(resolution):
-            x1 = v[r,t,0]
-            y1 = v[r,t,1]  # 生成的坐标点的坐标
+            x1 = v[r][t][0]
+            y1 = v[r][t][1]  # 生成的坐标点的坐标
             height = []
-            idex_list=[]
             for a in atom_position:
-                x2, y2, r2,idex = a[0], a[1], a[3],a[4]
+                x2, y2, r2 = a[0], a[1], a[3]
                 z2 = a[2] + abs(z_min)  # 我们将基平面平移至最底部，基平面此时的方程为：z=z_min
                 changdu = math.sqrt(math.pow((x1 - x2), 2) + math.pow((y1 - y2), 2))
                 if changdu <= r2:
                     point_height = math.sqrt(r2 * r2 - changdu * changdu) + z2
                     height.append(point_height)
-                    idex_list.append(idex)
                 else:
                     continue
             if len(height) == 0:
                 continue
             else:
                 point_height_max = max(height)
-                point_height_max_index=height.index(point_height_max)
+                # print(point_height_max)
                 points_intial[r][t] = point_height_max
-                point_idex=idex_list[point_height_max_index]
-                points_bool[point_idex,r,t]=1
     if show:
         plt.imshow(points_intial)
         plt.show()
-    with open("C:\\stm-result\\points_height.csv", "w+", newline='', encoding='GBK') as f1:
-        writer = csv.writer(f1, delimiter=',')
+    with open("points_height.csv", "w+", newline='', encoding='GBK') as f:
+        writer = csv.writer(f, delimiter=',')
         for i in points_intial:  # 对于每一行的，将这一行的每个元素分别写在对应的列中
             writer.writerow(i)
-    for i in range(atom_num):
-        arr_i=np.array(points_bool[i],dtype=bool)
-        with open(f"C:\\stm-result\\points_{str(i)}_bool.csv", "w+", newline='', encoding='GBK') as f2:
-            writer = csv.writer(f2, delimiter=',')
-            for i in arr_i:  # 对于每一行的，将这一行的每个元素分别写在对应的列中
-                writer.writerow(i)
+
 
 if __name__ == "__main__":
     # 由于原子半径的限制，目前只支持以下例子
-    file = "C:\\stm-result"
-    mkdir(file)  # 调用函数，创建文件夹
     smi = 'CNC(=O)N(N(CCCl)S(C)(=O)=O)S(C)(=O)=O'
     resolution = 400
     points_height_matrix(smi, resolution, show=True)
