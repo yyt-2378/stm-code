126205a0b6b20e06b2eda43bec8f36e0b694fb32
GilesLuo <luozhiyao933@126.com>
2022-07-26 12:19:55 +0100

    add sift and surf registration code

---
+++ image_registration.py
@@ -0,0 +1,58 @@
+import cv2
+import numpy as np
+
+
+def surf_kp(image):
+    height, width = image.shape[:2]
+    size = (int(width * 0.2), int(height * 0.2))
+    shrink = cv2.resize(image, size, interpolation=cv2.INTER_AREA)
+    gray_image = cv2.cvtColor(shrink, cv2.COLOR_BGR2GRAY)
+    surf = cv2.xfeatures2d_SURF.create()
+    kp, des = surf.detectAndCompute(gray_image, None)
+    return kp, des
+
+
+def sift_kp(image):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    sift = cv2.xfeatures2d_SIFT.create()
+    kp, des = sift.detectAndCompute(gray_image, None)
+    kp_image = cv2.drawKeypoints(gray_image, kp, None)
+    return kp_image, kp, des
+
+
+def get_good_match(des1, des2):
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
+    good = []
+    for m, n in matches:
+        if m.distance < 0.75 * n.distance:
+            good.append(m)
+    return good
+
+
+def siftImageAlignment(img1, img2):
+    _, kp1, des1 = sift_kp(img1)
+    _, kp2, des2 = sift_kp(img2)
+    goodMatch = get_good_match(des1, des2)
+    if len(goodMatch) > 4:
+        ptsA = np.float32([kp1[m.queryIdx].pt for m in goodMatch]).reshape(-1, 1, 2)
+        ptsB = np.float32([kp2[m.trainIdx].pt for m in goodMatch]).reshape(-1, 1, 2)
+        ransacReprojThreshold = 4
+        H, status = cv2.findHomography(ptsA, ptsB, cv2.RANSAC, ransacReprojThreshold)
+        imgOut = cv2.warpPerspective(img2, H, (img1.shape[1], img1.shape[0]),
+                                     flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+    else:
+        raise NotImplementedError
+    return imgOut, H, status
+
+
+if __name__ == "__main__":
+    import Utility
+
+    img1 = cv2.imread('1.jpg')
+    img2 = cv2.imread('2.jpg')
+    result, _, _ = siftImageAlignment(img1, img2)
+    allImg = np.concatenate((img1, img2, result), axis=1)
+    cv2.namedWindow('Result', cv2.WINDOW_NORMAL)
+    cv2.imshow('Result', allImg)
+    cv2.waitKey(0)
